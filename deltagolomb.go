4e49b079ee4ebc48d83c803dca4a7af2d30985ed
dave andersen <dave.andersen@gmail.com>
2012-01-23 00:43:27 -0500

    Not really much faster, but I prefer this formulation of bitLen to the one from arith.go;  it's guaranteed to take at most log2(word size) ops, which in this case is 5, compared to a max of 12 with the previous formulation.

--- deltagolomb.go
+++ deltagolomb.go
@@ -298,12 +298,24 @@ func (s *ExpGolombEncoder) addZeroBits(nzeros uint) {
 }
 
 // Computes the number of bits needed to represent a value.
-// Stolen from arith.go;  it's not exported there.
 func bitLen(x uint) (n int) {
-	for ; x >= 0x100; x >>= 8 {
+	if x >= 0x8000 {
+		x >>= 16
+		n += 16
+	}
+	if x >= 0x80 {
+		x >>= 8
 		n += 8
 	}
-	for ; x > 0; x >>= 1 {
+	if x >= 0x8 {
+		x >>= 4
+		n += 4
+	}
+	if x >= 0x2 {
+		x >>= 2
+		n += 2
+	}
+	if x >= 0x1 {
 		n++
 	}
 	return
